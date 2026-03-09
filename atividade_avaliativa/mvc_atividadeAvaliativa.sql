2d2237684b1b55bb7f9c526b73f50264af6daa9b
MariaClara <maria.c.silva470@aluno.senai.br>
2026-03-09 11:10:29 -0300

    atividade avaliativa

---
+++ atividade_avaliativa/mvc_atividadeAvaliativa.sql
@@ -0,0 +1,12 @@
+create database livros;
+use livros;
+
+CREATE TABLE livro(
+ID int auto_increment primary key,
+TITULO varchar (255),
+AUTOR varchar (255),
+ANO_PUBLICACAO year
+
+);
+
+SELECT *FROM livro;
